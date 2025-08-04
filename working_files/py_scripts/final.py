8e798dfa81c53b012520a1dfff6ff7ef9522ff3d
ntlewis1251 <ntlewis@astral.sciclone.wm.edu>
2025-08-04 12:40:09 -0400

    Working on file py script

---
+++ working_files/py_scripts/final.py
@@ -0,0 +1,60 @@
+"""
+Final py script for pycogs, WIP.
+
+Arguments order:  N (°), E (°), S (°), W (°), Resolution (m), Watershed Dataset Scale, Project Title, API Key
+"""
+
+"""
+**Script Goals**
+- Enter coordinate bounding box, RES, proj title (applied to all exp files with appropriate suffix)
+- Export files include:
+	- Relief map
+	- Slope map (similar but not the same!)
+	- Knickpoints mapped (maybe adjust delta ksn?  To isolate bedrock reaches.)
+	- Folders for each watershed w name
+		- For each stream, profile w sed cover represented by colorramp
+			- How to determine?  Albedo dataset is one possibility.  I.e reflectivity is modulated by stream cover.  Problem is trees.
+		- Profile w trimodal morphology represented (maybe, refer to chuck suggestion)
+	- CSVs of knickpoints
+		- To be included:
+		- elevation
+		- Coords
+		- Shapefile geology
+		- Nearest roadcut geology that is also within some threshold of elevation from knickpoint.  Do buffer around knickpoint select closest to elevation
+		- Stream cover type (to allow narrowing to bedrock)
+		- If BR, fracture data, if AP, bedding
+		- If not in AP or BR, Send back a message being like sorry dawg.
+		- Depth to mantle @ knickpoint.  Z score to average depth in region.
+		- Slope
+"""
+
+# Start of script
+# Import stack
+import sys
+from functions import * # Temporary, implimentation w/o seperate file TODO
+
+
+def maps():
+    """
+    Uses downloaded DEM object to export a TRI (Terrain Ruggedness Index, similar to local relief) map, a slope map, and knickpoints overlaying slope map (hued by Delta KSN) 
+    """
+
+def main():
+    # Takes args from command line and spits them back at you.  Lets you check your work ;).
+    # Saves to their own variables for use.
+    args = sys.argv
+    print(f'Script name: {args[0]}')
+    print(f'Bounds: {args[1]}, {args[2]}, {args[3]}, {args[4]}')
+    bounds = [args[1], args[2], args[3], args[4]]
+    print(f'Resolution: {args[5]}')
+    res = args[5]
+    print(f'Watershed Scale: {args[6]}')
+    wshed_scale = args[6]
+    print(f'Project Title: {args[7]}')
+    pj_title = args[7]
+    print(f'Api key supplied: {True if 8 in range(len(args)) else False}')
+    key = args[8]
+    pass
+
+if __name__ == '__main__':
+    main()
